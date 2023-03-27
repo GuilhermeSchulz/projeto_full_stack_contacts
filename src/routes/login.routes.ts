c644d061eb439cfad5e42cca26a13602eb0ac54c
Guilherme Schulz <gui.schulz@gmail.com>
2023-03-27 13:23:39 -0300

    cleaned files

--- src/routes/login.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { createSessionController } from '../controllers/login.controller'
-import { verifyLogin } from '../middlewares/verifyLoginData.middleware'
-
-
-export const loginRoutes = Router()
-
-loginRoutes.post('',verifyLogin, createSessionController)
-
